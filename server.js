18b3b3980b362df2e657027bf6a91c9331075ce0
Kayode Joshua <joshuakayode134@gmail.com>
2026-06-09 08:09:08 +0100

    CSS Update

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 /* ---------------- REGISTER ---------------- */
 
